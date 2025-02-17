de1bf4b9c4eac793539e953213d6d44269730c79
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-17 17:20:13 -0600

    wip

--- compiler+runtime/src/cpp/jank/runtime/perf.cpp
+++ compiler+runtime/src/cpp/jank/runtime/perf.cpp
@@ -4,47 +4,44 @@
 
 #include <jank/native_persistent_string/fmt.hpp>
 #include <jank/runtime/perf.hpp>
-#include <jank/runtime/visit.hpp>
+#include <jank/runtime/behaviors.hpp>
+#include <jank/runtime/obj/keyword.hpp>
+#include <jank/runtime/core/seq.hpp>
+#include <jank/runtime/core/to_string.hpp>
 #include <jank/runtime/context.hpp>
 
 namespace jank::runtime::perf
 {
   object_ptr benchmark(object_ptr const opts, object_ptr const f)
   {
-    auto const label(get(opts, __rt_ctx->intern_keyword("label").expect_ok()));
-    auto const label_str(to_string(label));
-    visit_object(
-      [](auto const typed_f, native_persistent_string const &label) {
-        using T = typename decltype(typed_f)::value_type;
+    auto const label(to_string(get(opts, __rt_ctx->intern_keyword("label").expect_ok())));
 
-        if constexpr(std::is_base_of_v<behavior::callable, T>)
-        {
-          ankerl::nanobench::Config config;
-          //config.mTimeUnitName = TODO
-          config.mOut = &std::cout;
+    auto const bs(object_behaviors(f));
+    if(bs.is_callable)
+    {
+      ankerl::nanobench::Config config;
+      //config.mTimeUnitName = TODO
+      config.mOut = &std::cout;
 
-          /* Larger things. */
-          config.mTimeUnit = std::chrono::milliseconds{ 1 };
-          config.mMinEpochIterations = 20;
-          config.mWarmup = 10;
+      /* Larger things. */
+      config.mTimeUnit = std::chrono::milliseconds{ 1 };
+      config.mMinEpochIterations = 20;
+      config.mWarmup = 10;
 
-          /* Smaller things. */
-          //config.mTimeUnit = std::chrono::nanoseconds{ 1 };
-          //config.mMinEpochIterations = 1000000;
-          //config.mWarmup = 1000;
+      /* Smaller things. */
+      //config.mTimeUnit = std::chrono::nanoseconds{ 1 };
+      //config.mMinEpochIterations = 1000000;
+      //config.mWarmup = 1000;
 
-          ankerl::nanobench::Bench().config(config).run(static_cast<std::string>(label), [&] {
-            auto const res(typed_f->call());
-            ankerl::nanobench::doNotOptimizeAway(res);
-          });
-        }
-        else
-        {
-          throw std::runtime_error{ fmt::format("not callable: {}", typed_f->to_string()) };
-        }
-      },
-      f,
-      label_str);
+      ankerl::nanobench::Bench().config(config).run(label, [&] {
+        auto const res(bs.call0(f));
+        ankerl::nanobench::doNotOptimizeAway(res);
+      });
+    }
+    else
+    {
+      throw std::runtime_error{ fmt::format("not callable: {}", bs.to_string(f)) };
+    }
     return obj::nil::nil_const();
   }
 }
