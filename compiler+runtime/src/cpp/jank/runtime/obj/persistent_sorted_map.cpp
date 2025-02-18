2369652376aaba5d7fe92407672dfd0e59cdb94f
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-18 15:57:41 -0600

    -visit

--- compiler+runtime/src/cpp/jank/runtime/obj/persistent_sorted_map.cpp
+++ compiler+runtime/src/cpp/jank/runtime/obj/persistent_sorted_map.cpp
@@ -4,8 +4,11 @@
 #include <jank/runtime/obj/persistent_sorted_map.hpp>
 #include <jank/runtime/obj/persistent_vector.hpp>
 #include <jank/runtime/obj/transient_sorted_map.hpp>
-#include <jank/runtime/visit.hpp>
+#include <jank/runtime/behaviors.hpp>
 #include <jank/runtime/core/seq.hpp>
+#include <jank/runtime/core/to_string.hpp>
+#include <jank/runtime/obj/nil.hpp>
+#include <jank/runtime/rtti.hpp>
 
 namespace jank::runtime::obj
 {
@@ -33,33 +36,26 @@ namespace jank::runtime::obj
 
   persistent_sorted_map_ptr persistent_sorted_map::create_from_seq(object_ptr const seq)
   {
-    return make_box<persistent_sorted_map>(visit_object(
-      [](auto const typed_seq) -> persistent_sorted_map::value_type {
-        using T = typename decltype(typed_seq)::value_type;
+    auto const bs(object_behaviors(seq));
+    if(!bs.is_seqable)
+    {
+      throw std::runtime_error{ fmt::format("Not seqable: {}", bs.to_string(seq)) };
+    }
 
-        if constexpr(behavior::seqable<T>)
-        {
-          runtime::detail::native_transient_sorted_map transient;
-          for(auto it(typed_seq->fresh_seq()); it != nullptr; it = runtime::next_in_place(it))
-          {
-            auto const key(it->first());
-            it = runtime::next_in_place(it);
-            if(!it)
-            {
-              throw std::runtime_error{ fmt::format("Odd number of elements: {}",
-                                                    typed_seq->to_string()) };
-            }
-            auto const val(it->first());
-            transient.insert_or_assign(key, val);
-          }
-          return transient.persistent();
-        }
-        else
-        {
-          throw std::runtime_error{ fmt::format("Not seqable: {}", typed_seq->to_string()) };
-        }
-      },
-      seq));
+    runtime::detail::native_transient_sorted_map transient;
+    //TODO next_in_place / first perf
+    for(auto it(bs.fresh_seq(seq)); it != nullptr; it = object_behaviors(it).next_in_place(it))
+    {
+      auto const key(object_behaviors(it).first(it));
+      it = object_behaviors(it).next_in_place(it);
+      if(!it)
+      {
+        throw std::runtime_error{ fmt::format("Odd number of elements: {}", bs.to_string(seq)) };
+      }
+      auto const val(object_behaviors(it).first(it));
+      transient.insert_or_assign(key, val);
+    }
+    return make_box<persistent_sorted_map>(transient.persistent());
   }
 
   object_ptr persistent_sorted_map::create_empty() const
