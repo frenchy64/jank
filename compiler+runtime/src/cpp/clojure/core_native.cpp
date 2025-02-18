ac01b9a97f3ce4526ba4899475e7cefb139e585d
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-18 10:00:27 -0600

    rm visit

--- compiler+runtime/src/cpp/clojure/core_native.cpp
+++ compiler+runtime/src/cpp/clojure/core_native.cpp
@@ -6,7 +6,21 @@
 #include <jank/runtime/core.hpp>
 #include <jank/runtime/context.hpp>
 #include <jank/runtime/behavior/callable.hpp>
-#include <jank/runtime/visit.hpp>
+#include <jank/runtime/behaviors.hpp>
+#include <jank/runtime/rtti.hpp>
+#include <jank/runtime/obj/atom.hpp>
+#include <jank/runtime/obj/delay.hpp>
+#include <jank/runtime/obj/integer_range.hpp>
+#include <jank/runtime/obj/jit_function.hpp>
+#include <jank/runtime/obj/keyword.hpp>
+#include <jank/runtime/obj/lazy_sequence.hpp>
+#include <jank/runtime/obj/multi_function.hpp>
+#include <jank/runtime/obj/native_function_wrapper.hpp>
+#include <jank/runtime/obj/persistent_hash_map.hpp>
+#include <jank/runtime/obj/persistent_hash_set.hpp>
+#include <jank/runtime/obj/persistent_sorted_map.hpp>
+#include <jank/runtime/obj/persistent_sorted_set.hpp>
+#include <jank/runtime/obj/range.hpp>
 
 namespace clojure::core_native
 {
@@ -522,20 +536,21 @@ jank_object_ptr jank_load_clojure_core_native()
         return obj::boolean::false_const();
       }
 
-      return visit_seqable(
-        [](auto const typed_rest, object_ptr const l) {
-          for(auto it(typed_rest->fresh_seq()); it != nullptr; it = it->next_in_place())
-          {
-            if(!equal(l, it->first()))
-            {
-              return obj::boolean::false_const();
-            }
-          }
+      auto const bs(object_behaviors(rest));
+      if(!bs.is_seqable)
+      {
+        throw std::runtime_error{ "not seqable: " + bs.to_code_string(rest) };
+      }
+      //TODO next_in_place / first perf
+      for(auto it(bs.fresh_seq(rest)); it != nullptr; it = object_behaviors(it).next_in_place(it))
+      {
+        if(!equal(l, object_behaviors(it).first(it)))
+        {
+          return obj::boolean::false_const();
+        }
+      }
 
-          return obj::boolean::true_const();
-        },
-        rest,
-        l);
+      return obj::boolean::true_const();
     };
     intern_fn_obj("=", fn);
   }
