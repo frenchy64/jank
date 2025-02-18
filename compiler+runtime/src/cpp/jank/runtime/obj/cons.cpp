bc186dc6992343a567e4233205e9960fb18efd21
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-18 16:29:08 -0600

    fmt

--- compiler+runtime/src/cpp/jank/runtime/obj/cons.cpp
+++ compiler+runtime/src/cpp/jank/runtime/obj/cons.cpp
@@ -75,7 +75,8 @@ namespace jank::runtime::obj
     for(auto it(fresh_seq()); it != nullptr;
         it = object_behaviors(it).next_in_place(it), seq = object_behaviors(seq).next_in_place(seq))
     {
-      if(seq == nullptr || !runtime::equal(object_behaviors(it).first(it), object_behaviors(seq).first(seq)))
+      if(seq == nullptr
+         || !runtime::equal(object_behaviors(it).first(it), object_behaviors(seq).first(seq)))
       {
         return false;
       }
