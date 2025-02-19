3f7797f6e140e702fbafa1ff710ddd03c7bdf26c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-02-19 16:30:06 -0600

    expect_object

--- compiler+runtime/src/cpp/jank/runtime/behaviors.cpp
+++ compiler+runtime/src/cpp/jank/runtime/behaviors.cpp
@@ -29,22 +29,22 @@ namespace jank::runtime
     if constexpr(behavior::object_like<T>)
     {
       this->is_object_like = true;
-      this->to_string = [](object_ptr const o) { return try_object<T>(o)->to_string(); };
+      this->to_string = [](object_ptr const o) { return expect_object<T>(o)->to_string(); };
       this->to_string_builder = [](object_ptr const o, util::string_builder &buff) {
-        return try_object<T>(o)->to_string(buff);
+        return expect_object<T>(o)->to_string(buff);
       };
-      this->to_code_string = [](object_ptr const o) { return try_object<T>(o)->to_code_string(); };
-      this->to_hash = [](object_ptr const o) { return try_object<T>(o)->to_hash(); };
+      this->to_code_string = [](object_ptr const o) { return expect_object<T>(o)->to_code_string(); };
+      this->to_hash = [](object_ptr const o) { return expect_object<T>(o)->to_hash(); };
       this->equal = [](object_ptr const lhs, object_ptr const rhs) {
-        return try_object<T>(lhs)->equal(*rhs);
+        return expect_object<T>(lhs)->equal(*rhs);
       };
-      this->base = [](object_ptr const o) { return try_object<T>(o)->base; };
+      this->base = [](object_ptr const o) { return expect_object<T>(o)->base; };
     }
     if constexpr(behavior::seqable<T>)
     {
       this->is_seqable = true;
-      this->seq = [](object_ptr const o) { return try_object<T>(o)->seq(); };
-      this->fresh_seq = [](object_ptr const o) { return try_object<T>(o)->fresh_seq(); };
+      this->seq = [](object_ptr const o) { return expect_object<T>(o)->seq(); };
+      this->fresh_seq = [](object_ptr const o) { return expect_object<T>(o)->fresh_seq(); };
     }
     if constexpr(behavior::sequential<T>)
     {
@@ -53,18 +53,18 @@ namespace jank::runtime
     if constexpr(behavior::sequenceable<T>)
     {
       this->is_sequenceable = true;
-      this->first = [](object_ptr const o) { return try_object<T>(o)->first(); };
-      this->next = [](object_ptr const o) { return try_object<T>(o)->next(); };
+      this->first = [](object_ptr const o) { return expect_object<T>(o)->first(); };
+      this->next = [](object_ptr const o) { return expect_object<T>(o)->next(); };
     }
     if constexpr(behavior::sequenceable_in_place<T>)
     {
       this->is_sequenceable_in_place = true;
-      this->next_in_place = [](object_ptr const o) { return try_object<T>(o)->next_in_place(); };
+      this->next_in_place = [](object_ptr const o) { return expect_object<T>(o)->next_in_place(); };
     }
     if constexpr(behavior::collection_like<T>)
     {
       this->is_collection = true;
-      this->empty = [](object_ptr const c) { return try_object<T>(c)->create_empty(); };
+      this->empty = [](object_ptr const c) { return expect_object<T>(c)->create_empty(); };
     }
     if constexpr(behavior::associatively_readable<T> && behavior::associatively_writable<T>)
     {
@@ -74,70 +74,70 @@ namespace jank::runtime
     {
       this->is_associatively_writable = true;
       this->assoc = [](object_ptr const m, object_ptr const k, object_ptr const v) {
-        return try_object<T>(m)->assoc(k, v);
+        return expect_object<T>(m)->assoc(k, v);
       };
       this->dissoc
-        = [](object_ptr const m, object_ptr const k) { return try_object<T>(m)->dissoc(k); };
+        = [](object_ptr const m, object_ptr const k) { return expect_object<T>(m)->dissoc(k); };
     }
     if constexpr(behavior::associatively_readable<T>)
     {
       this->is_associatively_readable = true;
-      this->get = [](object_ptr const m, object_ptr const k) { return try_object<T>(m)->get(k); };
+      this->get = [](object_ptr const m, object_ptr const k) { return expect_object<T>(m)->get(k); };
       this->get_default = [](object_ptr const m, object_ptr const k, object_ptr const d) {
-        return try_object<T>(m)->get(k, d);
+        return expect_object<T>(m)->get(k, d);
       };
       this->get_entry
-        = [](object_ptr const m, object_ptr const k) { return try_object<T>(m)->get_entry(k); };
+        = [](object_ptr const m, object_ptr const k) { return expect_object<T>(m)->get_entry(k); };
     }
     if constexpr(behavior::associatively_writable_in_place<T>)
     {
       this->is_associatively_writable_in_place = true;
       this->assoc_in_place = [](object_ptr const m, object_ptr const k, object_ptr const v) {
-        return try_object<T>(m)->assoc_in_place(k, v);
+        return expect_object<T>(m)->assoc_in_place(k, v);
       };
       this->dissoc_in_place = [](object_ptr const m, object_ptr const k) {
-        return try_object<T>(m)->dissoc_in_place(k);
+        return expect_object<T>(m)->dissoc_in_place(k);
       };
     }
     if constexpr(behavior::countable<T>)
     {
       this->is_countable = true;
-      this->count = [](object_ptr const o) { return try_object<T>(o)->count(); };
+      this->count = [](object_ptr const o) { return expect_object<T>(o)->count(); };
     }
     if constexpr(behavior::transientable<T>)
     {
       this->is_transientable = true;
-      this->to_transient = [](object_ptr const o) { return try_object<T>(o)->to_transient(); };
+      this->to_transient = [](object_ptr const o) { return expect_object<T>(o)->to_transient(); };
     }
     if constexpr(behavior::persistentable<T>)
     {
       this->is_persistentable = true;
-      this->to_persistent = [](object_ptr const o) { return try_object<T>(o)->to_persistent(); };
+      this->to_persistent = [](object_ptr const o) { return expect_object<T>(o)->to_persistent(); };
     }
     if constexpr(behavior::chunk_like<T>)
     {
       this->is_chunk_like = true;
-      this->chunk_next = [](object_ptr const o) { return try_object<T>(o)->chunk_next(); };
+      this->chunk_next = [](object_ptr const o) { return expect_object<T>(o)->chunk_next(); };
       this->chunk_next_in_place
-        = [](object_ptr const o) { return try_object<T>(o)->chunk_next_in_place(); };
+        = [](object_ptr const o) { return expect_object<T>(o)->chunk_next_in_place(); };
     }
     if constexpr(behavior::chunkable<T>)
     {
       this->is_chunkable = true;
-      this->chunked_first = [](object_ptr const o) { return try_object<T>(o)->chunked_first(); };
-      this->chunked_next = [](object_ptr const o) { return try_object<T>(o)->chunked_next(); };
+      this->chunked_first = [](object_ptr const o) { return expect_object<T>(o)->chunked_first(); };
+      this->chunked_next = [](object_ptr const o) { return expect_object<T>(o)->chunked_next(); };
     }
     if constexpr(behavior::metadatable<T>)
     {
       this->is_metadatable = true;
       this->with_meta
-        = [](object_ptr const o, object_ptr const m) { return try_object<T>(o)->with_meta(m); };
-      this->meta = [](object_ptr const o) { return try_object<T>(o)->meta; };
+        = [](object_ptr const o, object_ptr const m) { return expect_object<T>(o)->with_meta(m); };
+      this->meta = [](object_ptr const o) { return expect_object<T>(o)->meta; };
       this->get_meta = [](object_ptr const o) {
-        return try_object<T>(o)->meta.unwrap_or(obj::nil::nil_const());
+        return expect_object<T>(o)->meta.unwrap_or(obj::nil::nil_const());
       };
       this->set_meta = [](object_ptr const o, object_ptr const meta_obj) {
-        try_object<T>(o)->meta = behavior::detail::validate_meta(meta_obj);
+        expect_object<T>(o)->meta = behavior::detail::validate_meta(meta_obj);
         return meta_obj;
       };
     }
@@ -149,34 +149,34 @@ namespace jank::runtime
     {
       this->is_comparable = true;
       this->compare = [](object_ptr const lhs, object_ptr const rhs) {
-        return try_object<T>(lhs)->compare(*rhs);
+        return expect_object<T>(lhs)->compare(*rhs);
       };
     }
     if constexpr(behavior::function_like<T>)
     {
       this->is_function_like = true;
       //TODO start arguments from 1 instead of 0
-      this->call0 = [](object_ptr const o) { return try_object<T>(o)->call(); };
+      this->call0 = [](object_ptr const o) { return expect_object<T>(o)->call(); };
       this->call1
-        = [](object_ptr const o, object_ptr const a0) { return try_object<T>(o)->call(a0); };
+        = [](object_ptr const o, object_ptr const a0) { return expect_object<T>(o)->call(a0); };
       this->call2 = [](object_ptr const o, object_ptr const a0, object_ptr const a1) {
-        return try_object<T>(o)->call(a0, a1);
+        return expect_object<T>(o)->call(a0, a1);
       };
       this->call3
         = [](object_ptr const o, object_ptr const a0, object_ptr const a1, object_ptr const a2) {
-            return try_object<T>(o)->call(a0, a1, a2);
+            return expect_object<T>(o)->call(a0, a1, a2);
           };
       this->call4 = [](object_ptr const o,
                        object_ptr const a0,
                        object_ptr const a1,
                        object_ptr const a2,
-                       object_ptr const a3) { return try_object<T>(o)->call(a0, a1, a2, a3); };
+                       object_ptr const a3) { return expect_object<T>(o)->call(a0, a1, a2, a3); };
       this->call5 = [](object_ptr const o,
                        object_ptr const a0,
                        object_ptr const a1,
                        object_ptr const a2,
                        object_ptr const a3,
-                       object_ptr const a4) { return try_object<T>(o)->call(a0, a1, a2, a3, a4); };
+                       object_ptr const a4) { return expect_object<T>(o)->call(a0, a1, a2, a3, a4); };
       this->call6
         = [](object_ptr const o,
              object_ptr const a0,
@@ -184,7 +184,7 @@ namespace jank::runtime
              object_ptr const a2,
              object_ptr const a3,
              object_ptr const a4,
-             object_ptr const a5) { return try_object<T>(o)->call(a0, a1, a2, a3, a4, a5); };
+             object_ptr const a5) { return expect_object<T>(o)->call(a0, a1, a2, a3, a4, a5); };
       this->call7
         = [](object_ptr const o,
              object_ptr const a0,
@@ -193,7 +193,7 @@ namespace jank::runtime
              object_ptr const a3,
              object_ptr const a4,
              object_ptr const a5,
-             object_ptr const a6) { return try_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6); };
+             object_ptr const a6) { return expect_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6); };
       this->call8 = [](object_ptr const o,
                        object_ptr const a0,
                        object_ptr const a1,
@@ -203,7 +203,7 @@ namespace jank::runtime
                        object_ptr const a5,
                        object_ptr const a6,
                        object_ptr const a7) {
-        return try_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6, a7);
+        return expect_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6, a7);
       };
       this->call9 = [](object_ptr const o,
                        object_ptr const a0,
@@ -215,7 +215,7 @@ namespace jank::runtime
                        object_ptr const a6,
                        object_ptr const a7,
                        object_ptr const a8) {
-        return try_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6, a7, a8);
+        return expect_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6, a7, a8);
       };
       this->call10 = [](object_ptr const o,
                         object_ptr const a0,
@@ -228,10 +228,10 @@ namespace jank::runtime
                         object_ptr const a7,
                         object_ptr const a8,
                         object_ptr const a9) {
-        return try_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9);
+        return expect_object<T>(o)->call(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9);
       };
       this->get_arity_flags
-        = [](object_ptr const o) { return try_object<T>(o)->get_arity_flags(); };
+        = [](object_ptr const o) { return expect_object<T>(o)->get_arity_flags(); };
     }
     //TODO test for call1 arity
     if constexpr(std::same_as<T, obj::persistent_hash_set>
@@ -241,7 +241,7 @@ namespace jank::runtime
                  || std::same_as<T, obj::transient_hash_set> || std::same_as<T, obj::keyword>)
     {
       this->call1
-        = [](object_ptr const o, object_ptr const a0) { return try_object<T>(o)->call(a0); };
+        = [](object_ptr const o, object_ptr const a0) { return expect_object<T>(o)->call(a0); };
     }
     //TODO test for call2 arity
     if constexpr(std::same_as<T, obj::persistent_hash_map>
@@ -249,26 +249,26 @@ namespace jank::runtime
                  || std::same_as<T, obj::transient_hash_set> || std::same_as<T, obj::keyword>)
     {
       this->call2 = [](object_ptr const o, object_ptr const a0, object_ptr const a1) {
-        return try_object<T>(o)->call(a0, a1);
+        return expect_object<T>(o)->call(a0, a1);
       };
     }
     if constexpr(behavior::nameable<T>)
     {
       this->is_named = true;
-      this->get_name = [](object_ptr const o) { return try_object<T>(o)->get_name(); };
-      this->get_namespace = [](object_ptr const o) { return try_object<T>(o)->get_namespace(); };
+      this->get_name = [](object_ptr const o) { return expect_object<T>(o)->get_name(); };
+      this->get_namespace = [](object_ptr const o) { return expect_object<T>(o)->get_namespace(); };
     }
     if constexpr(behavior::derefable<T>)
     {
       this->is_derefable = true;
-      this->deref = [](object_ptr const o) { return try_object<T>(o)->deref(); };
+      this->deref = [](object_ptr const o) { return expect_object<T>(o)->deref(); };
     }
     if constexpr(behavior::indexable<T>)
     {
       this->is_indexable = true;
-      this->nth = [](object_ptr const o, object_ptr const i) { return try_object<T>(o)->nth(i); };
+      this->nth = [](object_ptr const o, object_ptr const i) { return expect_object<T>(o)->nth(i); };
       this->nth_default = [](object_ptr const o, object_ptr const i, object_ptr const d) {
-        return try_object<T>(o)->nth(i, d);
+        return expect_object<T>(o)->nth(i, d);
       };
     }
     if constexpr(behavior::map_like<T>)
@@ -282,30 +282,30 @@ namespace jank::runtime
     if constexpr(behavior::stackable<T>)
     {
       this->is_stackable = true;
-      this->peek = [](object_ptr const o) { return try_object<T>(o)->peek(); };
-      this->pop = [](object_ptr const o) { return try_object<T>(o)->pop(); };
+      this->peek = [](object_ptr const o) { return expect_object<T>(o)->peek(); };
+      this->pop = [](object_ptr const o) { return expect_object<T>(o)->pop(); };
     }
     if constexpr(behavior::number_like<T>)
     {
       this->is_number_like = true;
-      this->to_integer = [](object_ptr const o) { return try_object<T>(o)->to_integer(); };
-      this->to_real = [](object_ptr const o) { return try_object<T>(o)->to_real(); };
+      this->to_integer = [](object_ptr const o) { return expect_object<T>(o)->to_integer(); };
+      this->to_real = [](object_ptr const o) { return expect_object<T>(o)->to_real(); };
     }
     if constexpr(behavior::set_like<T> || behavior::associatively_readable<T>)
     {
       this->contains
-        = [](object_ptr const m, object_ptr const k) { return try_object<T>(m)->contains(k); };
+        = [](object_ptr const m, object_ptr const k) { return expect_object<T>(m)->contains(k); };
     }
     if constexpr(behavior::conjable<T>)
     {
       this->is_conjable = true;
-      this->conj = [](object_ptr const o, object_ptr const v) { return try_object<T>(o)->conj(v); };
+      this->conj = [](object_ptr const o, object_ptr const v) { return expect_object<T>(o)->conj(v); };
     }
     if constexpr(behavior::conjable_in_place<T>)
     {
       this->is_conjable_in_place = true;
       this->conj_in_place
-        = [](object_ptr const o, object_ptr const v) { return try_object<T>(o)->conj_in_place(v); };
+        = [](object_ptr const o, object_ptr const v) { return expect_object<T>(o)->conj_in_place(v); };
     }
   }
 
